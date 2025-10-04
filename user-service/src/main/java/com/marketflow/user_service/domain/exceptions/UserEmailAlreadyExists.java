b05d198064f9c0a2c4c34b70a681d041de9ac9ee
rfaromero12_ <rafael24.07.2001@gmail.com>
2025-10-04 21:24:38 +0200

    new exception

---
+++ user-service/src/main/java/com/marketflow/user_service/domain/exceptions/UserEmailAlreadyExists.java
@@ -0,0 +1,10 @@
+package com.marketflow.user_service.domain.exceptions;
+
+public class UserEmailAlreadyExists extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
