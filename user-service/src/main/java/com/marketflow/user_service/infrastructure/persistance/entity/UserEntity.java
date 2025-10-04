0ad0bb5779b924a751c8863e1e66c6a67d868f6e
rfaromero12_ <rafael24.07.2001@gmail.com>
2025-10-04 21:54:24 +0200

    Complete UserMapper and UserEntity

--- user-service/src/main/java/com/marketflow/user_service/infrastructure/persistance/entity/UserEntity.java
+++ user-service/src/main/java/com/marketflow/user_service/infrastructure/persistance/entity/UserEntity.java
@@ -1,5 +1,26 @@
 package com.marketflow.user_service.infrastructure.persistance.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+@Table(name = "Users")
 public class UserEntity {
 
+	@Id
+	private UUID userId;
+	
+	private String email;
+	
+	private String password;
+	
+	private String name;
+	
 }
--- user-service/src/main/java/com/marketflow/user_service/infrastructure/persistance/mapper/UserMapper.java
+++ user-service/src/main/java/com/marketflow/user_service/infrastructure/persistance/mapper/UserMapper.java
@@ -1,15 +1,29 @@
 package com.marketflow.user_service.infrastructure.persistance.mapper;
 
+import com.marketflow.user_service.domain.model.Email;
 import com.marketflow.user_service.domain.model.User;
+import com.marketflow.user_service.domain.model.UserId;
+import com.marketflow.user_service.domain.model.UserName;
+import com.marketflow.user_service.domain.model.UserPassword;
 import com.marketflow.user_service.infrastructure.persistance.entity.UserEntity;
 
 public class UserMapper {
 	public static UserEntity fromDomainToSchema(User user) {
-		return null;
+		return UserEntity.builder().
+				email(user.getEmail().getValue()).
+				name(user.getName().getValue()).
+				password(user.getPassword().getValue()).
+				userId(user.getUserId().getValue()).
+				build();
 		
 	}
 	
 	public static User fromSchemaToDomain(UserEntity entity) {
-		return null;
+		return User.builder().
+				email(new Email(entity.getEmail())).
+				name(new UserName(entity.getName())).
+				password(new UserPassword(entity.getPassword())).
+				userId(new UserId(entity.getUserId())).
+				build();
 	}
 }
