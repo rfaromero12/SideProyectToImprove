4b8ae0b5266f0757c19862bc83cb352900642a53
rfaromero12_ <rafael24.07.2001@gmail.com>
2025-10-04 21:25:00 +0200

    UserCommandHandler

---
+++ user-service/src/main/java/com/marketflow/user_service/aplication/commandHandler/RegisterUserCommandHandler.java
@@ -0,0 +1,42 @@
+package com.marketflow.user_service.aplication.commandHandler;
+
+import org.springframework.stereotype.Service;
+
+import com.marketflow.user_service.aplication.command.RegisterUserCommand;
+import com.marketflow.user_service.aplication.output.UserDTO;
+import com.marketflow.user_service.domain.exceptions.UserEmailAlreadyExists;
+import com.marketflow.user_service.domain.model.Email;
+import com.marketflow.user_service.domain.model.User;
+import com.marketflow.user_service.domain.model.UserName;
+import com.marketflow.user_service.domain.model.UserPassword;
+import com.marketflow.user_service.domain.port.UserPort;
+
+@Service
+public class RegisterUserCommandHandler {
+
+	private UserPort userPort;
+	
+	public RegisterUserCommandHandler(UserPort port) {
+		userPort = port;
+	}
+	
+	public void handle(RegisterUserCommand command) {
+		UserName name = new UserName(command.getName());
+		UserPassword password = new UserPassword(command.getPassword());		
+		UserPassword confirmPassword = new UserPassword(command.getConfirmPassword());		
+		Email userEmail =new Email(command.getEmail());
+		
+		if (userPort.findByEmail(userEmail.getValue()))
+			throw new UserEmailAlreadyExists();
+		
+		
+		if (!password.equals(confirmPassword))
+			throw new UserEmailAlreadyExists();
+		
+		User user = User.registerUser(name, password, userEmail);		
+		
+		userPort.save(user);
+		
+	}
+	
+}
